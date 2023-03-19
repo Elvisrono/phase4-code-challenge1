9c6aab9685a1e83af9d187ac74d43cac28c1b5cc
¨kericho¨ <lvisrono90@gmail.com>
2023-03-19 22:28:04 +0300

    completed challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :pizzas
+  resources :restaurants, only: [:index, :show, :update, :create, :destroy]
+  resources :pizzas, only: [:index, :update, :show, :create, :destroy]
+  resources :restaurant_pizzas, only: [:index, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
